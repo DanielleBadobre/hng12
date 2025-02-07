e4a998461b9180e9ceed705d21d5c965571b9d7b
Danielle Badobre <badobredanielle@gmail.com>
2025-02-07 21:25:56 +0000

    stage 1 task

---
+++ stage_1/app.py
@@ -0,0 +1,68 @@
+from flask_cors import CORS
+from flask import Flask, jsonify, request
+import requests
+
+app = flask(__name__)
+CORS(app)
+
+# functions
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+def is_perfect(n):
+    if n < 2:
+        return False
+    divisors = [i for i in range(1, n) if n % i == 0]
+    return sum(divisors) == n
+
+def is_armstrong(n):
+    digits = [int(d) for d in str(n)]
+    length = len(digits)
+    return sum(d**length for d in digits) == n
+
+def digit_sum(n):
+    return sum(int(d) for d in str(n))
+
+def get_fun_fact(n):
+    url = f"http://numbersapi.com{n}/math"
+    response = requests.get(url)
+    return response.text
+
+# API endpoint
+@app.route('/api/classify-number', methods=['GET'])
+def classify_number():
+    number = request.args.get('number')
+
+    if not number or not number.lstrip('-').isdigit():
+        return jsonify({"number": number, "error": True}), 400
+
+    number = int(number)
+    properties = []
+
+    # Check properties
+    if is_armstrong(number):
+        properties.append("armstrong")
+    if number % 2 == 0:
+        properties.append("even")
+    else:
+        properties.append("odd")
+
+    # Build response
+    response = {
+        "number": number,
+        "is_prime": is_prime(number),
+        "is_perfect": is_perfect(number),
+        "properties": properties,
+        "digit_sum": digit_sum(number),
+        "fun_fact": get_fun_fact(number)
+    }
+
+    return jsonify(response), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
